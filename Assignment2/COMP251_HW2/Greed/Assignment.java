96c194f158bcb4271bd9b42d085c94b3e53d72b6
chaggysen <sen.wang@mail.mcgill.ca>
2021-03-04 12:30:30 -0500

    q3 implemented

--- Assignment2/COMP251_HW2/Greed/Assignment.java
+++ Assignment2/COMP251_HW2/Greed/Assignment.java
@@ -1,27 +1,38 @@
 import java.util.*;
 
-class Assignment implements Comparator<Assignment>{
+class Assignment implements Comparator<Assignment> {
 	int number;
 	int weight;
 	int deadline;
-	
+
 	protected Assignment() {
 	}
-	
+
 	protected Assignment(int number, int weight, int deadline) {
 		this.number = number;
 		this.weight = weight;
 		this.deadline = deadline;
 	}
-	
+
 	/**
-	 * This method is used to sort to compare assignment objects for sorting. 
+	 * This method is used to sort to compare assignment objects for sorting.
 	 */
 	@Override
 	public int compare(Assignment a1, Assignment a2) {
-		// TODO Implement this
-		
-		
-		return 0;
+		// greedy decisions on weight
+		if (a1.weight > a2.weight) {
+			return -1;
+		} else if (a1.weight < a2.weight) {
+			return 1;
+		} else {
+			// weight are equal, greedy on deadline
+			if (a1.deadline < a2.deadline) {
+				return -1;
+			} else if (a1.deadline > a2.deadline) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
 	}
 }
--- Assignment2/COMP251_HW2/Greed/HW_Sched.java
+++ Assignment2/COMP251_HW2/Greed/HW_Sched.java
@@ -4,44 +4,53 @@ public class HW_Sched {
 	ArrayList<Assignment> Assignments = new ArrayList<Assignment>();
 	int m;
 	int lastDeadline = 0;
-	
+
 	protected HW_Sched(int[] weights, int[] deadlines, int size) {
-		for (int i=0; i<size; i++) {
+		for (int i = 0; i < size; i++) {
 			Assignment homework = new Assignment(i, weights[i], deadlines[i]);
 			this.Assignments.add(homework);
 			if (homework.deadline > lastDeadline) {
 				lastDeadline = homework.deadline;
 			}
 		}
-		m =size;
+		m = size;
 	}
-	
-	
+
 	/**
 	 * 
-	 * @return Array where output[i] corresponds to the assignment 
-	 * that will be done at time i.
+	 * @return Array where output[i] corresponds to the assignment that will be done
+	 *         at time i.
 	 */
 	public int[] SelectAssignments() {
-		//TODO Implement this
-		
-		//Sort assignments
-		//Order will depend on how compare function is implemented
+		// TODO Implement this
+
+		// Sort assignments
+		// Order will depend on how compare function is implemented
 		Collections.sort(Assignments, new Assignment());
-		
-		// If homeworkPlan[i] has a value -1, it indicates that the 
+
+		// If homeworkPlan[i] has a value -1, it indicates that the
 		// i'th timeslot in the homeworkPlan is empty
-		//homeworkPlan contains the homework schedule between now and the last deadline
+		// homeworkPlan contains the homework schedule between now and the last deadline
 		int[] homeworkPlan = new int[lastDeadline];
-		for (int i=0; i < homeworkPlan.length; ++i) {
+		for (int i = 0; i < homeworkPlan.length; ++i) {
 			homeworkPlan[i] = -1;
 		}
-	
-		
+
+		for (int i = 0; i < Assignments.size(); i++) {
+			int deadline = Assignments.get(i).deadline - 1;
+			// the day before the deadline is empty
+			if (homeworkPlan[deadline] == -1) {
+				homeworkPlan[Assignments.get(i).deadline - 1] = Assignments.get(i).number;
+			} else {
+				// the day before the dealine is taken. Check for previous days
+				for (int j = Assignments.get(i).deadline - 1; j >= 0; j--) {
+					if (homeworkPlan[j] == -1) {
+						homeworkPlan[j] = Assignments.get(i).number;
+					}
+				}
+			}
+		}
+
 		return homeworkPlan;
 	}
 }
-	
-
-
-
