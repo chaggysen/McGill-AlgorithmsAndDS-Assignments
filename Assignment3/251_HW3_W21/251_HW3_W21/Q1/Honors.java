e8e371065e719904c257bbb86b2c5d3a03e71760
chaggysen <sen.wang@mail.mcgill.ca>
2021-03-23 12:33:50 -0400

    q1 impleented

--- Assignment3/251_HW3_W21/251_HW3_W21/Q1/Honors.java
+++ Assignment3/251_HW3_W21/251_HW3_W21/Q1/Honors.java
@@ -22,9 +22,10 @@ public class Honors {
 	}
 
 	public static int min_moves(int[][] board) {
+		System.out.println("Starting bfs");
 		int[][] distance = new int[board.length][board[0].length];
-		Boolean[][] visited = new Boolean[board.length][board[0].length];
-		Queue<Node> queue = new LinkedList<>();
+		boolean[][] visited = new boolean[board.length][board[0].length];
+		Queue<Node> queue = new LinkedList<Node>();
 
 		Node startNode = new Node(0, 0);
 		distance[0][0] = 0;
@@ -33,8 +34,41 @@ public class Honors {
 
 		while (!queue.isEmpty()) {
 			// do BFS
+			Node currentNode = queue.poll();
+			System.out.println(currentNode.row + "," + currentNode.col);
+			int curMoves = board[currentNode.row][currentNode.col];
+
+			// append adjancent nodes to Queue
+			if (currentNode.col + curMoves < board[0].length && !visited[currentNode.row][currentNode.col + curMoves]) {
+				Node childNode = new Node(currentNode.row, currentNode.col + curMoves);
+				queue.add(childNode);
+				distance[childNode.row][childNode.col] = distance[currentNode.row][currentNode.col] + 1;
+				visited[childNode.row][childNode.col] = true;
+			}
+
+			if (currentNode.col - curMoves >= 0 && !visited[currentNode.row][currentNode.col - curMoves]) {
+				Node childNode = new Node(currentNode.row, currentNode.col - curMoves);
+				queue.add(childNode);
+				distance[childNode.row][childNode.col] = distance[currentNode.row][currentNode.col] + 1;
+				visited[childNode.row][childNode.col] = true;
+			}
+
+			if (currentNode.row + curMoves < board.length && !visited[currentNode.row + curMoves][currentNode.col]) {
+				Node childNode = new Node(currentNode.row + curMoves, currentNode.col);
+				queue.add(childNode);
+				distance[childNode.row][childNode.col] = distance[currentNode.row][currentNode.col] + 1;
+				visited[childNode.row][childNode.col] = true;
+			}
+
+			if (currentNode.row - curMoves >= 0 && !visited[currentNode.row - curMoves][currentNode.col]) {
+				Node childNode = new Node(currentNode.row - curMoves, currentNode.col);
+				queue.add(childNode);
+				distance[childNode.row][childNode.col] = distance[currentNode.row][currentNode.col] + 1;
+				visited[childNode.row][childNode.col] = true;
+			}
 		}
-		return 0; // placeholder
+		return distance[board.length - 1][board[0].length - 1] == 0 ? -1
+				: distance[board.length - 1][board[0].length - 1]; // placeholder
 	}
 
 	public void test(String filename) throws FileNotFoundException {
@@ -55,13 +89,18 @@ public class Honors {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-		Honors honors = new Honors();
-		honors.test(args[0]); // run 'javac Honors.java' to compile, then run 'java Honors testfilename'
+		System.out.println("Starting");
+		// Honors honors = new Honors();
+		// honors.test(args[0]); // run 'javac Honors.java' to compile, then run 'java
+		// Honors testfilename'
 
 		// or you can test like this
-		// int [][]board = {1,2,3}; // not actual example
-		// int answer = min_moves(board);
-		// System.out.println(answer);
+		int[][] board = { { 2, 1, 2, 0 }, { 1, 2, 0, 3 }, { 3, 1, 1, 3 }, { 1, 1, 2, 0 }, { 1, 1, 1, 0 } }; // not
+		// actual
+		// example
+		// int[][] board = { { 2, 2 }, { 2, 2 } };
+		int answer = min_moves(board);
+		System.out.println(answer);
 	}
 
 }
